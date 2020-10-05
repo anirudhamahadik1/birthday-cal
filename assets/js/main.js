b2048ea75ec167d79453f9677eb775e4a17fb5f0
anirudhamahdik1 <anirudhamahdik1@gmail.com>
2020-10-05 21:30:29 +0530

    Comments added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,41 @@
 function handleFormSubmit(e) {
     e.preventDefault()
 
+    // Get the data from the form.
     let form = e.target
     let data = JSON.parse(form["data"].value)
     let year = form["year"].value
 
-    // Filter data by year
+    // Filter data by year.
     let filData = data.filter(item => {
         let d = new Date(item.birthday)
         
         return year == d.getFullYear()
     })
 
-    // Sort filtered data by week days
+    // Sort filtered data by week days.
     let days = []
     filData.forEach(item => {
-        let d = new Date(item.birthday)
-        var name = item.name
-        var initials = name.match(/\b\w/g) || [];
+        // Get the Initials of the name.
+        let name = item.name
+        let initials = name.match(/\b\w/g) || [];
         item.initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
         
+        // Push the object data as per day of week.
+        let d = new Date(item.birthday)
         if(days[d.getDay()] === undefined) {
-            days[d.getDay()] = []
+            days[d.getDay()] = [] // Initialize with an empty array.
         }
         days[d.getDay()].push(item)
     });
 
-    let cal = []
+    let cal = [] // Initialize Calendar component array.
+    // Some 
     let colors = ["red", "green", 'blue', "cyan", "magenta", "yellow", "red", "green", 'blue', "cyan", "magenta", "yellow", "red", "green", 'blue', "cyan", "magenta", "yellow"]
     for(i = 0; i <= 6; i++) {
-        let people = []
+        let people = [] // Initailize people component array.
         if(days[i] !== undefined) {
+            // CSS column class logic.
             let count = days[i].length
             let col = "col"
             if(count <= 1) {
@@ -44,11 +49,14 @@ function handleFormSubmit(e) {
                         } else {
                                 col = "col"
                             }
+            
+            // Add people component to array.
             days[i].forEach((item, index) => {
                 people[index] = `<div class="${col} people" style="background-color:${colors[index]};">${item.initials}</div>`
             })
         }
 
+        // Add calendar component to array.
         cal[i] = `<div class="col">
             <div class="card">
                 <div class="card-header">
@@ -65,5 +73,5 @@ function handleFormSubmit(e) {
         </div>`
     }
 
-    document.querySelector('.calendar').innerHTML = cal.join("")
+    document.querySelector('.calendar').innerHTML = cal.join("") // Display to DOM element.
 }
