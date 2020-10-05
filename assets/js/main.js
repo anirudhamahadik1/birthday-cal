019158c2bee61b804c2c986fe293bd41754072f6
anirudhamahdik1 <anirudhamahdik1@gmail.com>
2020-10-05 11:18:26 +0530

    Initial Commit

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+function handleFormSubmit(e) {
+    e.preventDefault()
+
+    let form = e.target
+    let data = JSON.parse(form["data"].value)
+    let year = form["year"].value
+
+    // Filter data by year
+    let filData = data.filter(item => {
+        let d = new Date(item.birthday)
+        
+        return year == d.getFullYear()
+    })
+
+    // Sort filtered data by week days
+    let days = []
+    filData.forEach(item => {
+        let d = new Date(item.birthday)
+        var name = item.name
+        var initials = name.match(/\b\w/g) || [];
+        item.initials = ((initials.shift() || '') + (initials.pop() || '')).toUpperCase();
+        
+        if(days[d.getDay()] === undefined) {
+            days[d.getDay()] = []
+        }
+        days[d.getDay()].push(item)
+    });
+
+    let cal = []
+    let colors = ["red", "green", 'blue', "cyan", "magenta", "yellow", "red", "green", 'blue', "cyan", "magenta", "yellow", "red", "green", 'blue', "cyan", "magenta", "yellow"]
+    for(i = 0; i <= 6; i++) {
+        let people = []
+        if(days[i] !== undefined) {
+            let count = days[i].length
+            let col = "col"
+            if(count <= 1) {
+                col = "col-12"
+            } else if(count <= 4) {
+                    col = "col-6"
+                } else if(count <= 9) {
+                        col = "col-4"
+                    } else if(count <= 16) {
+                            col = "col-3"
+                        } else {
+                                col = "col"
+                            }
+            days[i].forEach((item, index) => {
+                people[index] = `<div class="${col} people" style="background-color:${colors[index]};">${item.initials}</div>`
+            })
+        }
+
+        cal[i] = `<div class="col">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Mon</h4>
+                </div>
+                <div class="card-body">
+                    <div class="card-body-inner">
+                        <div class="row">
+                            ${people.join("")}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+
+    document.querySelector('.calendar').innerHTML = cal.join("")
+}
